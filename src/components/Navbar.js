121f69353b9da5d575d0a9c55e6ecbb03fc2695d
Ankush <168534792+Ankush135@users.noreply.github.com>
2026-01-08 13:36:18 +0530

    intial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <a href="/">Posts</a>
+      <a href="/users">Users</a>
+      <a href="/notifications">Notifications</a>
+    </nav>
+  );
+}
+
+export default Navbar;
